9499ddefbc30b01619a8b9eeccb44eb0c8aba616
erickcasita <erickm.casanova@gmail.com>
2021-12-24 18:20:52 -0600

    Agregando validacion para fila vacia

--- src/public/assets/js/multiselect.js
+++ src/public/assets/js/multiselect.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function(){
+    let tmp = ['o'];
     $('#tipomovimientosalida').hide();
     $('#lbltipomovimientosalida').hide();
     $('#movimiento').on('change', function (){
@@ -20,21 +21,36 @@ $(document).ready(function(){
         }
         
  });
- $(document).on("click", ".plus", function(){		            
+ $(document).on("click", ".plus", function(){
    
-    let cantidad = $('#cantidad').val();
-    let idproducto = $('#producto').val();
-    let producto =  $('#producto option:selected').text();
-    let val = idproducto+"$"+cantidad;
-    let txt = producto + " = "+cantidad;
-    //$('.lista').append('<li class="list-group-item"  >'+txt+'<input type="hidden" name="Hola" value='+val +' / > <a href="#" class="btn btn-outline-danger btn-rounded  delete"><i class="fas fa-trash"></i></a></li>'); 
-    $('.tbl-articulos').append('<tr><th scope="row">'+producto+'<input type="hidden" name="tabla" value='+val+' /></th><td>'+cantidad+'</td><td><a href="#" class="btn btn-outline-danger btn-rounded  delete"><i class="fas fa-trash"></i></a></td></tr>')    
+    if($('#cantidad').val() === ''){
+        Swal.fire(
+            'Atención!',
+            'Ingrese una cantidad',
+            'info'
+          )       
+    }else{
+        let cantidad = $('#cantidad').val();
+        let idproducto = $('#producto').val();
+        let producto =  $('#producto option:selected').text();
+        let val = idproducto+"$"+cantidad;
+        let txt = producto + " = "+cantidad;
+        //$('.lista').append('<li class="list-group-item"  >'+txt+'<input type="hidden" name="Hola" value='+val +' / > <a href="#" class="btn btn-outline-danger btn-rounded  delete"><i class="fas fa-trash"></i></a></li>'); 
+        $('.tbl-articulos').append('<tr><th scope="row">'+producto+'<input type="hidden" name="tabla" value='+val+' /></th><td>'+cantidad+'</td><td><a href="#" class="btn btn-outline-danger btn-rounded  delete"><i class="fas fa-trash"></i></a></td></tr>');  
+
+    }
+ 
  });
 
  $(document).on("click", ".delete", function(event){
     event.preventDefault();
     $(this).closest("tr").remove();
-    
 });
+
+function checkId (id) {
+	let ids = document.querySelectorAll('.tbl-articulos th[for="producto"]');
+    console.log(ids);
+  return [].filter.call(ids, th => th.textContent === producto).length === 1;
+}
 
 })
