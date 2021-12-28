93004b1c077dbfdf45fbbb3d006bd639c1a575a5
erickcasita <erickm.casanova@gmail.com>
2021-12-28 10:21:21 -0600

    Validacion cantidad vacia, cantidad en 0 y articulo repetido

--- src/public/assets/js/multiselect.js
+++ src/public/assets/js/multiselect.js
@@ -1,9 +1,9 @@
 
-$(document).ready(function(){
-    let tmp = ['o'];
+$(document).ready(function () {
+    //Funcion para ocultar los select segun el movimiento ENTRADA | SALIDA
     $('#tipomovimientosalida').hide();
     $('#lbltipomovimientosalida').hide();
-    $('#movimiento').on('change', function (){
+    $('#movimiento').on('change', function () {
         if ($("#movimiento option:selected").text() == 'SALIDA') {
             $('#tipomovimientoentrada').hide();
             $('#tipomovimientoentrada').prop('disabled', true);
@@ -11,7 +11,7 @@ $(document).ready(function(){
             $('#tipomovimientosalida').show();
             $('#tipomovimientosalida').prop('disabled', false);
             $('#lbltipomovimientosalida').show();
-        }else{
+        } else {
             $('#tipomovimientoentrada').show();
             $('#tipomovimientoentrada').prop('disabled', false);
             $('#lbltipomovimientoentrada').show();
@@ -19,38 +19,50 @@ $(document).ready(function(){
             $('#tipomovimientosalida').prop('disabled', true);
             $('#lbltipomovimientosalida').hide();
         }
-        
- });
- $(document).on("click", ".plus", function(){
-   
-    if($('#cantidad').val() === ''){
-        Swal.fire(
-            'Atención!',
-            'Ingrese una cantidad',
-            'info'
-          )       
-    }else{
-        let cantidad = $('#cantidad').val();
-        let idproducto = $('#producto').val();
-        let producto =  $('#producto option:selected').text();
-        let val = idproducto+"$"+cantidad;
-        let txt = producto + " = "+cantidad;
-        //$('.lista').append('<li class="list-group-item"  >'+txt+'<input type="hidden" name="Hola" value='+val +' / > <a href="#" class="btn btn-outline-danger btn-rounded  delete"><i class="fas fa-trash"></i></a></li>'); 
-        $('.tbl-articulos').append('<tr><th scope="row">'+producto+'<input type="hidden" name="tabla" value='+val+' /></th><td>'+cantidad+'</td><td><a href="#" class="btn btn-outline-danger btn-rounded  delete"><i class="fas fa-trash"></i></a></td></tr>');  
 
+    });
+    //Funcion para agregar un elemento
+    $(document).on("click", ".plus", function () {
+
+
+        if ($('#cantidad').val() === '' || $('#cantidad').val() === '0') {
+            Swal.fire(
+                'Atención!',
+                'Ingrese una cantidad',
+                'info'
+            )
+        } else {
+            let cantidad = $('#cantidad').val();
+            let idproducto = $('#producto').val();
+            let producto = $('#producto option:selected').text();
+            let val = idproducto + "$" + cantidad;
+            let txt = producto + " = " + cantidad;
+
+
+            //Validando row repetidos....
+
+            if (checkId(producto)) {
+                Swal.fire(
+                    'Atención!',
+                    'El artículo: ' + producto + " ya está en la lista",
+                    'info'
+                )
+            } else {
+                $('#tbl-articulos').append('<tr><td for="tbl" >' + producto + '<input type="hidden" name="tabla" value=' + val + ' /></td><td>' + cantidad + '</td><td><a href="#" class="btn btn-outline-danger btn-rounded  delete"><i class="fas fa-trash"></i></a></td></tr>');
+            }
+        }
+
+    });
+    //Funcion para validar si hay un articulo repetido
+    function checkId(id) {
+        let ids = document.querySelectorAll('#tbl-articulos td[for="tbl" ]');
+
+        return [].filter.call(ids, td => td.textContent === id).length === 1;
     }
- 
- });
+    //Funcion para eliminar un articulo
+    $(document).on("click", ".delete", function (event) {
+        event.preventDefault();
+        $(this).closest("tr").remove();
+    });
 
- $(document).on("click", ".delete", function(event){
-    event.preventDefault();
-    $(this).closest("tr").remove();
 });
-
-function checkId (id) {
-	let ids = document.querySelectorAll('.tbl-articulos th[for="producto"]');
-    console.log(ids);
-  return [].filter.call(ids, th => th.textContent === producto).length === 1;
-}
-
-})
