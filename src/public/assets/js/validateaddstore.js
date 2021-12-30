8a403eb4d786f17776997af07448cb8b366f935f
erickcasita <erickm.casanova@gmail.com>
2021-12-29 18:48:49 -0600

    Validacion modales agregar tienda

---
+++ src/public/assets/js/validateaddstore.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    //código para validar si hay un registro vacio.
+
+    $("#agregar").click(function(){
+        
+        let nombretienda = $('#nombretienda').val();
+        let calle = $('#calle').val();
+        let numero = $('#numero').val();
+        let colonia = $('#colonia').val();
+
+        if(nombretienda === '' || calle === '' || numero === '' || colonia === ''){
+            Swal.fire(
+                'Atención!',
+                'Todos los datos son requeridos',
+                'info'
+            )
+            return false;
+        }
+
+    });
+
+    $("#editar").click(function(){
+        
+        let nombretienda = $('#nombretiendaeditar').val();
+        let calle = $('#calleeditar').val();
+        let numero = $('#numeroeditar').val();
+        let colonia = $('#coloniaeditar').val();
+
+        if(nombretienda === '' || calle === '' || numero === '' || colonia === ''){
+            Swal.fire(
+                'Atención!',
+                'Todos los datos son requeridos',
+                'info'
+            )
+            return false;
+        }
+
+    });
+}); 
