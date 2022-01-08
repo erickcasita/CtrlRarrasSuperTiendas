b1c108f7d19ea7776e2cfb5b1ef78cf8d0e156ba
erickcasita <erickm.casanova@gmail.com>
2022-01-07 18:56:39 -0600

    Apartado acerca de

---
+++ src/public/assets/js/acercade.js
@@ -0,0 +1,11 @@
+$("#acercade").click(function(){
+    Swal.fire({
+        title: 'Rarras Super Tiendas S.A. DE C.V.',
+        text: 'Analista: Ing. Ramón Alberto Vargas Quino \n Programador: Ing. Erick Manuel Ramírez Casanova Todos los Derechos Reservados © 2022 V1.0',
+        imageUrl: '/assets/img/logorarras.png',
+        imageWidth: 400,
+        imageHeight: 300,
+        imageAlt: 'Custom image',
+      });
+
+});
